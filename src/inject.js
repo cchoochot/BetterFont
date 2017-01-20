48cdec9cdf803a8e75bfed7f52aa6e18d93bc88c
Chakraphan <chakraphan.choochot@gmail.com>
2017-01-20 15:12:38 +0700

    First simple version

---
+++ src/inject.js
@@ -0,0 +1,26 @@
+/*global chrome, FontFace*/
+(function (document) {
+	'use strict';
+
+	var baseurl = chrome.extension.getURL('/fonts/');
+
+	var addFontFunc = function (face, fontfile) {
+		var fontface = new FontFace(face, 'url(' + baseurl + fontfile + ')');
+		document.fonts.add(fontface);
+		fontface.loaded.then(function () {});
+	};
+
+	var processFunc = function () {
+		addFontFunc('Arial', 'Roboto-Regular.ttf');
+		addFontFunc('Helvetica', 'texgyreheros-regular.woff');
+		addFontFunc('Helvetica Neue', 'texgyreheros-regular.woff');
+
+		document.fonts.ready.then(function () {
+			console.info('Better Font: ready to rock!');
+		});
+	};
+
+	if ("Win32" === navigator.platform) {
+		processFunc();
+	}
+}(document));
